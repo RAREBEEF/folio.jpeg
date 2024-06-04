0e4e468b3bf378470d8d47352ec22bf2faa16193
RAREBEEF <rarebeef@RAREBEEFui-MacBookPro.local>
2024-06-04 15:54:09 +0900

    private key를 서버사이드 변수로 설정 및 Analytics 비활성

--- src/fb.ts
+++ src/fb.ts
@@ -24,4 +24,4 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
